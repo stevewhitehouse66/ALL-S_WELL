221f0949f0f1f1777ca6c16e13f9eb16f1ad2285
steve whitehouse <stewhites@blueyonder.co.uk>
2024-03-01 17:56:31 +0000

    add migrations for comment /reviews

--- articles/models.py
+++ articles/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 STATUS = ((0, "Draft"), (1, "Published"))
+
+
 class Article(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -12,3 +14,25 @@ class Article(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Review(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="review")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviewer")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    
