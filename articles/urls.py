1cf507119ef143b24982605d3df83c3e4138949d
steve whitehouse <stewhites@blueyonder.co.uk>
2024-03-05 15:56:50 +0000

    add Martin!

--- articles/urls.py
+++ articles/urls.py
@@ -3,10 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    #path('', views.ArticleList.as_view(), name='home'),
-    #path('events', views.EventList.as_view(), name='events'),
-    #path('<slug:slug>/', views.article_detail, name='article_detail'),
-    #path('<slug:slug>/', views.event_detail, name='event_detail'),
     path('', views.ArticleList.as_view(), name='home'),
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
     path('events/', views.EventList.as_view(), name='events'),
