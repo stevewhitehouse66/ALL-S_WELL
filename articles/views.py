a3051fbc325b2bcee42877645a6b10cadac332d8
Manjula Lal <manjula_lal@hotmail.com>
2024-03-06 18:29:28 +0000

    added js

--- articles/views.py
+++ articles/views.py
@@ -4,9 +4,6 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Article, Event, Comment
 from .forms import CommentForm, ReviewForm
-
-
-
 # Create your views here.
 def comment_edit(request, slug, comment_id):
     """
@@ -14,21 +11,21 @@ def comment_edit(request, slug, comment_id):
     """
     if request.method == "POST":
 
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+        queryset = Article.objects.filter(status=1)
+        article = get_object_or_404(queryset, slug=slug)
         comment = get_object_or_404(Comment, pk=comment_id)
         comment_form = CommentForm(data=request.POST, instance=comment)
 
         if comment_form.is_valid() and comment.author == request.user:
             comment = comment_form.save(commit=False)
-            comment.post = post
+            comment.post = comment
             comment.approved = False
             comment.save()
             messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
         else:
             messages.add_message(request, messages.ERROR, 'Error updating comment!')
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+    return HttpResponseRedirect(reverse('article_detail', args=[slug]))
 
 class ArticleList(ListView):
     """
